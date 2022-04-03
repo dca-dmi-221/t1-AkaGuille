97c650bb9fb4b7ca3fb8453fe02f2eb86905d68b
Guillermo Valencia Mosquera <1107834256@u.icesi.edu.co>
2022-04-03 17:20:33 -0500

    ;(

--- MP3 player.js
+++ MP3 player.js
@@ -10,3 +10,27 @@ function playSong(){
         song.play();
     }
 }
+
+function pauseSong(){
+    if (song.isPlaying()) {
+        song.pause();
+    }
+}
+
+function nextSong(){
+    song.stop();
+    if (currentSongIndex < songDefault.length) {
+        song = songDefault[currentSongIndex++].link;
+        currentSongIndex = currentSongIndex++;
+    } else {
+        song = songDefault[0].link;
+        currentSongIndex = 1;
+    }
+    song.play();
+    console.log(currentSongIndex)
+}
+
+function previousSong() {
+
+}
+
--- sketch.js
+++ sketch.js
@@ -1,23 +1,26 @@
 let frame1, frame2;
 let songDefault = [];
-let currentSongIndex = 0;
-let song = songDefault[0].link;
+let currentSongIndex = 1;
+let song;
+let slider;
+
+
 function preload() {
   soundFormats('mp3', 'ogg');
   songDefault =[
     {
       name: "Flight Of The Bumblebee",
-      link: loadSound("../songs/Flight Of The Bumblebee.mp3")
+      link: loadSound("songs/Flight Of The Bumblebee.mp3")
     },
 
     {
       name: "Ride Of The Valkyries",
-      link: loadSound("../songs/Ride Of The Valkyries.mp3")
+      link: loadSound("songs/Ride Of The Valkyries.mp3")
     },
 
     {
       name: "William Tell Overture",
-      link: loadSound("../songs/William Tell Overture.mp3")
+      link: loadSound("songs/William Tell Overture.mp3")
     },
   ]
 }
@@ -27,18 +30,30 @@ function setup() {
   frame1 = loadImage("../Images/Frame1.png");
   frame2 = loadImage("../Images/Frame2.png");
 
+  song = songDefault[0].link;
+
   //Buttons
   const bPlay = createButton("PLAY");
-  bPlay.position(620, 626);
+  bPlay.position(600, 626);
   bPlay.mousePressed(playSong);
 
-  
+  const bPause = createButton("PAUSE");
+  bPause.position(500, 626);
+  bPause.mousePressed(pauseSong);
 
   const bNext = createButton("NEXT");
   bNext.position(700, 626);
+  bNext.mousePressed(nextSong);
 
   const bPrevious = createButton("PREVIOUS");
-  bPrevious.position(520, 626);
+  bPrevious.position(400, 626);
+  bPrevious.mousePressed(previousSong);
+
+  //Slider
+  slider = createSlider(0, 255, 100);
+  slider.style('width', "100px");
+  slider.position(1000, 626);
+  
 
 }
 
