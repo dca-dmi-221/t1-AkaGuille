ca84f06d87b21867dd93b1f8f17cdc21cd5dfcdc
Guillermo Valencia Mosquera <1107834256@u.icesi.edu.co>
2022-04-03 11:06:38 -0500

    :(

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
-let frame1;
-let frame2;
+let frame1, frame2;
+let play, pause, next, previous;
 
 function setup() {
   createCanvas(1280, 720);
-  frame1 = new App (loadImage("../Images/Frame1.png"), 640, 360);
-  frame2 = new App (loadImage("../Images/Frame2.png"), 640, 360);
+  frame1 = loadImage("../Images/Frame1.png");
+  frame2 = loadImage("../Images/Frame2.png");
+
+  //Buttons
+  const play = createButton('');
+  play.elt.className = 'playButton';
+  play.position(620, 626);
+
+
 }
 
 function draw() {
   background(220);
-  frame2.show();
+  imageMode(CENTER);
+  image(frame1, 640, 360);
+  image(frame2, 640, 360);
+  imageMode(CORNER);
 }
 
